aa25b0b2b0bc3719c93cf3a1572ce96c616de758
tmhr1009 <aichitead21107@gmail.com>
2022-08-10 16:21:09 +0900

    土台角度をcan0x00で送る

--- src/read_kakudo_arm_ichi/read_kakudo_arm_ichi.ino
+++ src/read_kakudo_arm_ichi/read_kakudo_arm_ichi.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "pid.h"
+#include "motor.h"
+#include <MsTimer2.h>
+#include <FlexCAN.h>
+#include <kinetis_flexcan.h>
+#include <FastLED.h>
 
+#define DODAI_MIN 20
+#define DODAI_MAX 590
+
+#define NUM_LEDS 5
+#define DATA_PIN 21
+CRGB leds[NUM_LEDS];
+
+FlexCAN CANTransmitter(1000000);
+static CAN_message_t dodai_msg;
+
+int now_dodai = 0; //土台 現在位置 (エンコーダーから)
+
+void setup() {
+  CANTransmitter.begin();
+  pinMode(10, INPUT);
+  dodai_msg.id = 0x00;
+  dodai_msg.len = 8;
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  MsTimer2::set(2, timerInt);
+  MsTimer2::start();
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  now_dodai = map(analogRead(A0), DODAI_MIN, DODAI_MAX, 0, 600);
 
+  dodai_msg.buf[0] = now_dodai;
+  delay(5);
+}
+
+void timerInt() {
+  CANTransmitter.write(dodai_msg);
+  FastLED.show();
 }
